6cc815db800c01f536c3951541fad932058bf88b
gongjs <382663074@qq.com>
2022-01-05 14:12:19 +0800

    feat: 新增 egg-validate 验证参数

--- app/extend/helper.ts
+++ app/extend/helper.ts
@@ -1,18 +1,28 @@
-import {  Context } from 'egg'
-
+import { Context } from 'egg';
 interface RespType {
-    ctx: Context
-    res?: any
-    msg?: string
+  ctx: Context;
+  res?: any;
+  msg?: string;
 }
-
+interface ErrorRespType {
+  ctx: Context;
+  errno: number;
+  msg?: string;
+}
 export default {
-    success({ ctx, res, msg }: RespType) {
-        ctx.body = {
-            code: 0,
-            msg: msg || '请求成功',
-            data: res
-        }
-        ctx.ststus = 200
-    }
-}
+  success({ ctx, res, msg }: RespType) {
+    ctx.body = {
+      errno: 0,
+      data: res ? res : null,
+      message: msg ? msg : '请求成功',
+    };
+    ctx.status = 200;
+  },
+  error({ ctx, msg, errno }: ErrorRespType) {
+    ctx.body = {
+      errno,
+      message: msg ? msg : '请求错误',
+    };
+    ctx.status = 200;
+  },
+};
