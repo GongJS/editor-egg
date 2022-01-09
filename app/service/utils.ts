db17e5cc82f5e96e38552cb9fc3edbcab2345155
gongjs <382663074@qq.com>
2022-01-09 15:30:31 +0800

    feat: 新增 body 样式

--- app/service/utils.ts
+++ app/service/utils.ts
@@ -6,15 +6,26 @@ import * as ReactDOMServer from 'react-dom/server';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { LImage, LText, LShape, BootstrapComponent } from 'lego-bricks-react';
 export default class UserService extends Service {
+  propsToStyle(props = {}) {
+    const keys = Object.keys(props);
+    const styleArr = keys.map(key => {
+      const formatKey = key.replace(/[A-Z]/g, c => `-${c.toLocaleLowerCase()}`);
+      // fontSize -> font-size
+      const value = props[key];
+      return `${formatKey}: ${value}`;
+    });
+    return styleArr.join(';');
+  }
   async renderToPageData(query: { id: string, uuid: string }) {
     const work = await this.ctx.model.Work.findOne(query).lean();
     if (!work) {
       throw new Error('work not exist');
     }
     const { title, desc, content } = work;
+    const { components = [], props = {} } = content;
     const reactEle = React.createElement('div', {
       className: 'final-page',
-    }, content.components.map((component, index) => {
+    }, components.map((component, index) => {
       return React.createElement(BootstrapComponent, {
         props: component.props,
         name: component.name,
@@ -22,10 +33,12 @@ export default class UserService extends Service {
       });
     }));
     const html = ReactDOMServer.renderToString(reactEle);
+    const bodyStyle = this.propsToStyle(props);
     return {
       html,
       title,
       desc,
+      bodyStyle,
     };
   }
 }
